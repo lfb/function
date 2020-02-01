86754c7eaca7453015ba926be6bb309877b9b636
Fengbo Liang <fengboliang@foxmail.com>
2020-02-01 22:36:37 +0800

    新增instanceof注释

--- _instanceof.js
+++ _instanceof.js
@@ -1,5 +1,6 @@
 /**
  * 模拟实现 instanceof 操作符
+ * instanceof 运算符用于检测构造函数的 prototype 属性是否出现在某个实例对象的原型链上
  *
  * @param L
  * @param R
@@ -7,11 +8,14 @@
  * @private
  */
 function _instanceof(L, R) {
+  // 传入是 undefined 或 null 直接 返回 false
   if (L == null) {
     return false
   }
 
+  // 取左边对象的 __proto__
   L = L.__proto__
+  // 取右边构造函数的 prototype
   R = R.prototype
 
   while (true) {
@@ -25,12 +29,19 @@ function _instanceof(L, R) {
   }
 }
 
+/**
+ * 测试
+ */
 var arr = []
 
 function F() {
 }
 
-console.log(_instanceof(arr, Array))
-console.log(_instanceof(123, Object))
-console.log(_instanceof(null, Object))
-console.log(_instanceof(undefined, Object))
+console.log(_instanceof(arr, Array)) // true
+console.log(_instanceof(123, Object)) // true
+console.log(_instanceof(null, Object)) // false
+console.log(_instanceof(undefined, Object)) // false
+console.log(_instanceof({}, F)) // false
+console.log(_instanceof({}, Object)) // true
+console.log(_instanceof(new F(), Object)) // true
+
