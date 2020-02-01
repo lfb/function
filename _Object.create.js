4ffe8f8e4e2b094a360f675f3c1e772c7d6f0bcc
Fengbo Liang <fengboliang@foxmail.com>
2020-02-01 22:44:17 +0800

    新增模拟实现Object.create注释

--- _Object.create.js
+++ _Object.create.js
@@ -1,6 +1,8 @@
 /**
  * 模拟实现 Object.create() 方法
  *
+ * Object.create()方法创建一个新对象，使用现有的对象来提供新创建的对象的__proto__
+ *
  * @param proto
  * @return {*}
  * @private
@@ -15,6 +17,36 @@ function _ObjectCreate(proto) {
 
   F.prototype = proto
 
-  return F()
+  return new F()
 }
+
+/**
+ * 测试：完成一个继承例子
+ */
+function Parent(name) {
+  this.name = name
+}
+
+Parent.prototype.getName = function () {
+  console.log(this.name)
+}
+
+function Child(name, age) {
+  Parent.call(this, name)
+  this.age = age
+}
+
+Child.prototype = _ObjectCreate(Parent.prototype)
+
+var o = new Child('lynn', 16)
+
+console.log(o.name)
+console.log(o.age)
+console.log(o) // { name: 'lynn', age: 16 }
+o.getName() // lynn
+console.log(o instanceof Child) // true
+console.log(o instanceof Parent) // true
+console.log(o instanceof Object) // true
+
+
 
