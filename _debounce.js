3fa6a3a9c5ee9baeba1e2a99746b53e121fc6520
Fengbo Liang <fengboliang@foxmail.com>
2020-02-02 00:03:28 +0800

    新增防抖与节流的注释代码

--- _debounce.js
+++ _debounce.js
@@ -1,37 +1,61 @@
 /**
- * 防抖
+ * 防抖的原理就是：你尽管触发事件，但是我一定在事件触发 n 秒后才执行，
+ * 如果你在一个事件触发的 n 秒内又触发了这个事件，那我就以新的事件的时间为准，n 秒后才执行，
+ * 总之，就是要等你触发完事件 n 秒内不再触发事件，我才执行
  *
- * @param fn
- * @param delay
- * @param immediate
- * @private
+ * 也可以简单的概括：无论触发多少次，最后一次触发说了算
+ *
+ * 实现原理：定时器 --> 每次进入创建一个定时器，
+ * --> 如果在规定时间之前还有进入触发，则清除上一次的定时器，再重新创建新定时器，
+ * --> 直到时间到了，就进行触发调用函数。
+ *
+ * @param fn 传入的函数
+ * @param delay 触发描述，单位毫秒
+ * @param immediate 是否立即调用 true | false
+ * @return {function(): *}
  */
 function _debounce(fn, delay, immediate) {
-  var timeout
+  var timeout, result
 
-  return function () {
+  var _debounce = function () {
+    var context = this
+    var args = arguments
+
     if (timeout) {
       clearTimeout(timeout)
     }
 
-    var context = this
-    var args = arguments
-
     if (immediate) {
       var callNow = !timeout
+
+      // 到了时间再去重新执行
+      // clearTimeout(timeout) 是暂停了时器，但定时器还是存在
+      // timeout = null 才是真正的清除定时器
       timeout = setTimeout(function () {
         timeout = null
       }, delay)
 
       if (callNow) {
-        fn.apply(context, args)
+        // 可能存在返回值
+        result = fn.apply(context, args)
       }
-
 
     } else {
       timeout = setTimeout(function () {
+        // setTimeout 是异步执行，需要等待秒数
+        // 这时返回 result 会一直是 undefined
         fn.apply(context, args)
       }, delay)
     }
+
+    return result
   }
+
+  // 可以取消节流
+  _debounce.cancel = function () {
+    clearTimeout(timeout)
+    timeout = null
+  }
+
+  return _debounce
 }
--- _throttle.js
+++ _throttle.js
@@ -1,8 +1,21 @@
 /**
- * 定时器实现节流
+ * 节流的原理很简单：如果你持续触发事件，每隔一段时间，只执行一次事件
  *
- * @param fn
- * @param delay
+ * 第一种：使用时间戳实现：
+ *   首先初始化一个时间戳 previous 为 0，然后创建一个当前执行的时间戳 now，
+ *   当 now - previous 大于设置的时间 delay 时，执行调用函数，如果小于，则不执行。
+ *
+ * 第二种：定时器实现节流：
+ *   开始初始化一个定时器变量 timeout，判断 timeout 定时器值为空的时候，开启一个新定时器且赋值给 timeout，
+ *   当 timeout 定时器设置的时间到了， 就会执行函数，执行完毕后就把 timeout 清空，重新轮回。
+ *
+ * 区别：
+ *   第一种事件会立刻执行，第二种事件会在 n 秒后第一次执行。
+ *   第一种事件停止触发后没有办法再执行事件，第二种事件停止触发后依然会再执行一次事件
+ *
+ *
+ * @param fn 执行的回调函数
+ * @param delay 设置的触发时间
  * @private
  */
 function _throttle(fn, delay) {
@@ -23,8 +36,9 @@ function _throttle(fn, delay) {
 
 /**
  * 时间戳实现节流
- * @param fn
- * @param delay
+ *
+ * @param fn 执行的回调函数
+ * @param delay 设置的触发时间
  * @return {Function}
  * @private
  */
@@ -46,8 +60,8 @@ function _throttle2(fn, delay) {
 /**
  * 定时器与时间戳相结合优化实现节流
  *
- * @param fn
- * @param delay
+ * @param fn 执行的回调函数
+ * @param delay 设置的触发时间
  * @private
  */
 function _throttle3(fn, delay) {
@@ -58,6 +72,7 @@ function _throttle3(fn, delay) {
     var context = this
     var now = +new Date()
 
+    // 离下一次触发的时间
     var remaining = delay - (now - previous)
     if (remaining <= 0 || remaining > delay) {
       if (timeout) {
